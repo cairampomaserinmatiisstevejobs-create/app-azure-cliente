43852de852bfab1d25b8df862d4e050bc60a83f1
Alan Cairampoma <acairampoma@gmail.com>
2026-04-21 02:31:36 -0500

    chore: add gitignore remove target and idea folders

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO cliente (nombre, apellido, email, telefono, estado) VALUES
-('Alan',  'Cairampoma', 'alan@empresa.com',   '999111222', 'ACTIVO'),
-('Maria', 'Lopez',      'maria@empresa.com',  '999333444', 'ACTIVO'),
-('Carlos','Rios',       'carlos@empresa.com', '999555666', 'INACTIVO');
